4576711b989fa3d314cfba6ae9bcd5aeef9fe8de
Mathias Bragagia <mathias.bragagia@epitech.eu>
2025-04-21 19:40:53 +0200

    Add photo, change description, and make it better overall

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 function SectionTitle({ children }: { children: React.ReactNode }) {
   return <h2 className="text-2xl mb-2 text-gray-500 josefin">{children}</h2>;
 }
@@ -5,6 +7,25 @@ function SectionTitle({ children }: { children: React.ReactNode }) {
 function SectionContent({ children }: { children: React.ReactNode }) {
   return (
     <div className="text-gray-800 space-y-4 font-light pl-6">{children}</div>
+  );
+}
+
+function ArrowRight() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+      />
+    </svg>
   );
 }
 
@@ -16,37 +37,43 @@ export default function HomePage() {
 
       <header className="mb-12">
         <div className="flex flex-col md:flex-row items-start md:items-end justify-between md:mt-8 mb-8">
-          <h1 className="prata text-4xl md:text-5xl my-2 font-bold">
-            Mathias
-            <br />
-            Bragagia
-          </h1>
-
-          <div className="flex flex-col">
-            <div className="crimson italic text-md md:max-w-xs my-2 text-justify text-gray-700 flex flex-col">
-              <a href="tel:+33781208307" className="hover:text-blue-600">
-                07 81 20 83 07
-              </a>
-              <a
-                href="mailto:mathias@bragagia.com"
-                className="hover:text-blue-600"
-              >
-                mathias@bragagia.com
-              </a>
-              <a
-                href="https://www.linkedin.com/in/bragagiamathias"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-blue-600"
-              >
-                linkedin.com/in/bragagiamathias
-              </a>
-            </div>
+          <div className="flex flex-row my-2 items-end gap-4">
             <div>
-              <p className="prata italic text-sm md:max-w-xs my-2 text-justify text-gray-700">
-                Freelance Senior Full-Stack Developer
-              </p>
+              <Image
+                src="/me-way-too-corpo.jpg"
+                alt="Mathias Bragagia"
+                width={1895}
+                height={3237}
+                className="w-16 inline sepiatify"
+              />
             </div>
+
+            <h1 className="prata text-4xl md:text-5xl font-bold">
+              <br />
+              Mathias
+              <br />
+              Bragagia
+            </h1>
+          </div>
+
+          <div className="crimson italic text-md md:max-w-xs my-2 text-justify text-gray-700 flex flex-col">
+            <a href="tel:+33781208307" className="hover:text-blue-600">
+              07 81 20 83 07
+            </a>
+            <a
+              href="mailto:mathias@bragagia.com"
+              className="hover:text-blue-600"
+            >
+              mathias@bragagia.com
+            </a>
+            <a
+              href="https://www.linkedin.com/in/bragagiamathias"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-600"
+            >
+              linkedin.com/in/bragagiamathias
+            </a>
           </div>
         </div>
 
@@ -59,115 +86,78 @@ export default function HomePage() {
         <SectionTitle>Summary</SectionTitle>
         <SectionContent>
           <p>
-            Full-Stack Developer & Entrepreneur in Paris, fluent in React.js,
-            React Native, Node.js, and Golang. My mission? Turning cool ideas
-            into seriously impactful digital products.
+            <span className="font-semibold">
+              Freelance Senior Full-Stack Developer & Entrepreneur in Paris
+            </span>
+            , my job is to turn your cool ideas into seriously impactful digital
+            products.
           </p>
+
           <p>
-            My superpower comes from having worn both PM and CTO hats – I get
-            the big picture strategy *and* the nitty-gritty code.
+            As I have worn both Technical and Product hats, I bring a unique
+            perspective that combines deep technical expertise with a strategic
+            vision for product development.
           </p>
+
           <p>
-            What I bring to your project: <br />
+            <span className="font-semibold">In short:</span>
+            <br />
             <span className="mr-1 font-semibold">→</span>Taking complex web &
-            mobile apps (PWA, SSR, React Native) from zero to one, end-to-end.{" "}
+            mobile apps (PWA, SSR, Native) from zero to one, end-to-end
             <br />
             <span className="mr-1 font-semibold">→</span>Mastery of the modern
             tech stack: Cloud (AWS, Vercel), CI/CD, Docker, databases (Postgres,
-            NoSQL), Microservices, queues, comprehensive testing... and more!{" "}
+            NoSQL), Microservices, queues, comprehensive testing...
             <br />
             <span className="mr-1 font-semibold">→</span>Clear communication and
-            strong teamwork vibes (fully bilingual French/English).
+            strong teamwork (fully bilingual French/English)
           </p>
-          <p>
-            Ready to inject some energy and serious tech/product know-how into
-            your next project? Let's talk!
-          </p>
-        </SectionContent>
-      </section>
-
-      {/* Skills Section */}
-      <section className="mb-10">
-        {/* Section Title - Use josefin class */}
-        <SectionTitle>Core Skills</SectionTitle>
-        <SectionContent>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-x-6 gap-y-5 text-gray-800 text-sm">
-            <ul className="space-y-1">
-              <li className="font-medium text-gray-700 text-base mb-1">
-                Languages & Frameworks:
-              </li>
-              <li className="font-light">JavaScript, TypeScript</li>
-              <li className="font-light">React.js, React Native</li>
-              <li className="font-light">Node.js, Golang</li>
-              <li className="font-light">HTML5, CSS3</li>
-            </ul>
-            <ul className="space-y-1">
-              <li className="font-medium text-gray-700 text-base mb-1">
-                Architecture & Ops:
-              </li>
-              <li className="font-light">Microservices</li>
-              <li className="font-light">LLMOps</li>
-              <li className="font-light">Progressive Web Apps (PWA)</li>
-              <li className="font-light">Server-Side Rendering (SSR)</li>
-              <li className="font-light">CI/CD (e.g., GitHub Actions)</li>
-              <li className="font-light">Docker</li>
-            </ul>
-            <ul className="space-y-1">
-              <li className="font-medium text-gray-700 text-base mb-1">
-                Cloud & Databases:
-              </li>
-              <li className="font-light">AWS, Vercel, Scalingo</li>
-              <li className="font-light">PostgreSQL, NoSQL (e.g., MongoDB)</li>
-              <li className="font-light">GraphQL, REST APIs</li>
-              <li className="font-light">Kafka</li>
-            </ul>
-            <ul className="space-y-1">
-              <li className="font-medium text-gray-700 text-base mb-1">
-                Product & Methodology:
-              </li>
-              <li className="font-light">Agile Development</li>
-              <li className="font-light">Product Management</li>
-              <li className="font-light">User Research & Design</li>
-              <li className="font-light">Testing (Unit, Integration)</li>
-            </ul>
-            <ul className="space-y-1">
-              <li className="font-medium text-gray-700 text-base mb-1">
-                Languages:
-              </li>
-              <li className="font-light">French (Native)</li>
-              <li className="font-light">English (Full Professional)</li>
-            </ul>
-          </div>
         </SectionContent>
       </section>
 
       {/* Side Projects Section */}
       <section className="mb-10">
-        {/* Section Title - Use josefin class */}
-        <SectionTitle>Side Projects & Initiatives</SectionTitle>
+        <SectionTitle>Portfolio - Pro & Side Projects</SectionTitle>
         <SectionContent>
-          <ul className="space-y-3 text-gray-800 font-light">
-            <li>
-              <strong className="font-medium text-gray-800">
-                Sunrise Briefing:
-              </strong>
-              AI-generated daily news summaries delivered each morning. Explores
-              LLMs for content synthesis and delivery automation.
-            </li>
+          <div className="grid grid-cols-2 gap-x-6 gap-y-5 text-gray-800 text-sm">
+            <ul className="space-y-1 group border border-gray-300 p-3 rounded-md">
+              <li className="text-lg font-semibold text-gray-800 josefin mb-1 flex flex-row justify-between items-center">
+                <div>Sunrise Briefing</div>
+                <div className="text-xs text-gray-500 flex h-4 w-4 opacity-0 group-hover:opacity-100">
+                  <ArrowRight />
+                </div>
+              </li>
+              <li className="text-sm text-gray-500 mb-1 font-light">
+                AI-generated daily news summaries delivered each sunrise.
+              </li>
+            </ul>
 
-            <li>
-              <strong className="font-medium text-gray-800">YouWise:</strong>
-              Spaced repetition system (SRS) application focused on learning
-              business and self-development book summaries. Features learning
-              algorithms and progress tracking.
-            </li>
+            <ul className="space-y-1 group border border-gray-300 p-3 rounded-md">
+              <li className="text-lg font-semibold text-gray-800 josefin mb-1 flex flex-row justify-between items-center">
+                <div>YouWise</div>
+                <div className="text-xs text-gray-500 opacity-0 group-hover:opacity-100">
+                  #offline
+                </div>
+              </li>
+              <li className="text-sm text-gray-500 mb-1 font-light">
+                Spaced repetition system (SRS) application focused on learning
+                business and self-development book summaries.
+              </li>
+            </ul>
 
-            <li>
-              <strong className="font-medium text-gray-800">Todobeast:</strong>
-              Project management-focused todo application with emphasis on
-              prioritization, clean UI/UX, and efficient task handling.
-            </li>
-          </ul>
+            <ul className="space-y-1 group border border-gray-300 p-3 rounded-md">
+              <li className="text-lg font-semibold text-gray-800 josefin mb-1 flex flex-row justify-between items-center">
+                <div>Todobeast</div>
+                <div className="text-xs text-gray-500 opacity-0 group-hover:opacity-100">
+                  #offline
+                </div>
+              </li>
+              <li className="text-sm text-gray-500 mb-1 font-light">
+                Project management-focused todo application with emphasis on
+                prioritization, clean UI/UX, and efficient task handling.
+              </li>
+            </ul>
+          </div>
         </SectionContent>
       </section>
 
@@ -250,7 +240,7 @@ export default function HomePage() {
               </ul>
               <h3 className="text-lg font-semibold text-gray-800 mt-3 josefin">
                 Product Engineer
-              </h3>{" "}
+              </h3>
               {/* Added margin top */}
               <p className="text-sm text-gray-500 mb-1 font-light">
                 Liberkeys | Paris, France | Dec 2018 - Jan 2021 (2 years 2
@@ -291,27 +281,27 @@ export default function HomePage() {
                 <li>
                   <strong className="font-medium text-gray-700">
                     Development Manager | Teacher at Play2Code
-                  </strong>{" "}
+                  </strong>
                   (Alsace Digitale): Organized coding workshops, managed teams,
                   created tutorials.
                 </li>
                 <li>
                   <strong className="font-medium text-gray-700">
                     Secretary & Resource Manager
-                  </strong>{" "}
+                  </strong>
                   (Student office - Epitech): Managed team, organized events,
                   handled budget and partnerships.
                 </li>
                 <li>
                   <strong className="font-medium text-gray-700">
                     Pedagogical Assistant
-                  </strong>{" "}
+                  </strong>
                   (Epitech): Mentored students, evaluated projects.
                 </li>
                 <li>
                   <strong className="font-medium text-gray-700">
                     C# Developer (Internship)
-                  </strong>{" "}
+                  </strong>
                   (VOSSLOH): Developed a C# library for railway simulation
                   testing.
                 </li>
@@ -364,6 +354,61 @@ export default function HomePage() {
           <p className="text-gray-800 font-light">
             Prix Alsace Digitale - Hacking Industry Camp 2016
           </p>
+        </SectionContent>
+      </section>
+
+      {/* Skills Section */}
+      <section className="mb-10">
+        {/* Section Title - Use josefin class */}
+        <SectionTitle>Core Skills</SectionTitle>
+        <SectionContent>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-x-6 gap-y-5 text-gray-800 text-sm">
+            <ul className="space-y-1">
+              <li className="font-medium text-gray-700 text-base mb-1">
+                Languages & Frameworks
+              </li>
+              <li className="font-light">JavaScript, TypeScript</li>
+              <li className="font-light">React.js, React Native</li>
+              <li className="font-light">Node.js, Golang</li>
+              <li className="font-light">HTML5, CSS3</li>
+            </ul>
+            <ul className="space-y-1">
+              <li className="font-medium text-gray-700 text-base mb-1">
+                Architecture & Ops
+              </li>
+              <li className="font-light">Microservices</li>
+              <li className="font-light">LLMOps</li>
+              <li className="font-light">Progressive Web Apps (PWA)</li>
+              <li className="font-light">Server-Side Rendering (SSR)</li>
+              <li className="font-light">CI/CD (e.g., GitHub Actions)</li>
+              <li className="font-light">Docker</li>
+            </ul>
+            <ul className="space-y-1">
+              <li className="font-medium text-gray-700 text-base mb-1">
+                Cloud & Databases
+              </li>
+              <li className="font-light">AWS, Vercel, Scalingo</li>
+              <li className="font-light">PostgreSQL, NoSQL (e.g., MongoDB)</li>
+              <li className="font-light">GraphQL, REST APIs</li>
+              <li className="font-light">Kafka</li>
+            </ul>
+            <ul className="space-y-1">
+              <li className="font-medium text-gray-700 text-base mb-1">
+                Product & Methodology
+              </li>
+              <li className="font-light">Agile Development</li>
+              <li className="font-light">Product Management</li>
+              <li className="font-light">User Research & Design</li>
+              <li className="font-light">Testing (Unit, Integration)</li>
+            </ul>
+            <ul className="space-y-1">
+              <li className="font-medium text-gray-700 text-base mb-1">
+                Languages
+              </li>
+              <li className="font-light">French (Native)</li>
+              <li className="font-light">English (Full Professional)</li>
+            </ul>
+          </div>
         </SectionContent>
       </section>
     </div>
